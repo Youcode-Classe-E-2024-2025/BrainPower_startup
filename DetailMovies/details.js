1b7568ab08cc400a6c0ea96b5e5f9d5a0ef30eb9
Asmae <asmaeelhamzaoui77@gmail.com>
2024-11-13 08:17:13 +0100

    modification des paramètres du védio des films

--- DetailMovies/details.js
+++ DetailMovies/details.js
@@ -9,17 +9,32 @@ document.addEventListener("DOMContentLoaded", () => {
   
       detailsContainer.innerHTML = `
         <div class="film-info-container">
-          <img src="${film.image}" alt="${film.title}" class="film-image">
-          <div class="film-info">
+        <div class="film-left">
+               <img src="${film.image}" alt="${film.title}" class="film-image">
+                <div id="movieAchat">   
+                    <h3 style="color:aliceblue">9.5</h3>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <h3 style="color:aliceblue">130DH</h3>
+                    <button style="width:100px; background-color: aliceblue;color: black;">Buy Now</button>
+                    <i class="fa-solid fa-cart-plus" style="color: #ffffff; font-size: 20px;"></i>
+                  </div> 
+        </div>
+         
+               
+           <div class="film-info">
             <h1><b>${film.title}</b></h1>
             <p><strong>Description:</strong> <br>${film.description}</p>
             <p><strong>Genre:</strong><br> ${film.genre}</p>
             <p><strong>Date de production:</strong><br> ${film.releaseDate}</p>
             <p><strong>Time:</strong><br> ${film.time}</p>
             <p><strong>Author:</strong><br> ${film.author}</p>
-            <video src="${film.video}"loop autoplay muted class="film-video"></video>
+            <div class="video-container">
+            <video src="${film.video}" loop autoplay muted ;class="film-video"></video>
+            </div>
+            
           </div>
         </div>
+                
       `;
     } else {
       document.getElementById("film-details").innerHTML = "<p>Film non trouvé.</p>";
