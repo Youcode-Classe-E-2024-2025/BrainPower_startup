8dab6d6f0d44dea6d8d6335e2a8407b47961411b
Asmae <asmaeelhamzaoui77@gmail.com>
2024-11-14 12:12:53 +0100

    modification des CSS du page détail

--- asset/DetailMovie/details.js
+++ asset/DetailMovie/details.js
@@ -8,27 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
       const detailsContainer = document.getElementById("film-details");
   
       detailsContainer.innerHTML = `
-        <div class="film-info-container">
-        <div class="film-left">
-               <img src="${film.image}" alt="${film.name}" class="film-image">
-                 <div id="movieAchat">   
-                    <h3 style="color:aliceblue">9.5</h3>
-                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
-                    <h3 style="color:aliceblue">${film.price}DH</h3>
-                    <button style="width:100px; background-color: aliceblue;color:orange; border-radius:5px">Buy Now</button>
-                    <i class="fa-solid fa-cart-plus" style="color: #ffffff; font-size: 20px;"></i>
-                  </div> 
-        </div>
-           <div class="film-info">
-            <h1><b>${film.name}</b></h1>
-            <p><strong>Description:</strong> <br>${film.text}</p>
-            <p><strong>Genre:</strong><br> ${film.genre}</p>
-            <p><strong>Date de production:</strong><br> ${film.date}</p>
-            <p><strong>Time:</strong><br> ${film.dure}</p>
-            <p><strong>Author:</strong><br> ${film.autheur}</p>
-            <iframe width="800" height="300" src="${film.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     <div class="film-info-container">
+          <div class="film-left">
+             <img src="${film.image}" alt="${film.name}" class="film-image">
+                    <div id="movieAchat">   
+                       <h3 style="color:aliceblue">9.5</h3>
+                       <i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                       <h3 style="color:aliceblue">${film.price}DH</h3>
+                       <button style="width:100px; background-color: aliceblue;color:orange; border-radius:5px">Buy Now</button>
+                      <i class="fa-solid fa-cart-plus" style="color: #ffffff; font-size: 20px;"></i>
+                    </div> 
           </div>
-        </div>
+          <div class="film-info">
+              <h1><b>${film.name}</b></h1>
+              <p><strong>Description:</strong> <br>${film.text}</p>
+              <p><strong>Genre:</strong><br> ${film.genre}</p>
+              <p><strong>Date de production:</strong><br> ${film.date}</p>
+              <p><strong>Time:</strong><br> ${film.dure}</p>
+              <p><strong>Author:</strong><br> ${film.autheur}</p>
+              <iframe width="100%" height="300" src="${film.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+     </div>
                 
       `;
     } else {
